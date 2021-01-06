5fc605ad92784708c7c0dc5ef5ebeedb7c64f809
Lorenzo Callegari <Callegari.lorenzo@gmail.com>
2021-01-07 00:12:06 +0100

    Reduced stdout spam

--- .github/workflows/update-bot/generateJson.py
+++ .github/workflows/update-bot/generateJson.py
@@ -10,14 +10,12 @@ if __name__ == '__main__':
     games_list = list()
     page = 0
     while True:
-        print(f'Page {page}')
         r = requests.get(MAIN_URL, params={
-                         'page': page, 'sort_by': 'crack_date', 'is_cracked': 'true', 'is_aaa': 'true'})
+                         'page': page, 'sort_by': 'crack_date', 'is_cracked': 'true'})
         data = json.loads(r.text)
         if not data:
             break
         for game in data:
-            del game['isAAA']
             del game['NFOsCount']
             del game['commentsCount']
             del game['followersCount']
@@ -33,4 +31,4 @@ if __name__ == '__main__':
 
     with open('crackwatch-games-list.json', 'w', encoding='utf8') as f:
         f.write(json.dumps(games_list, indent=4, ensure_ascii=False))
-    print(f'Completed, elapsed time: {round(time.time() - t, 2)} seconds')
+    print(f'Fetched {page} pages, elapsed time: {round(time.time() - t, 2)} seconds')
